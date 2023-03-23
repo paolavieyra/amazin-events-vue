e54f708c45bbe30ab94529f53e1f0bd4c03db23c
Paola Vieyra <paolavieyra.cba@gmail.com>
2023-03-22 23:39:36 -0300

    terminado index

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,7 +3,12 @@ const apiUrl = "https://mindhub-xj03.onrender.com/api/amazing"
 createApp({
     data() {
         return {
-            eventos: []
+            eventos: [],
+            checkbox: [],
+            inputCheck: [],
+            eventosFiltrados: [],
+            texto:""
+            
 
         }
     },
@@ -14,13 +19,22 @@ createApp({
             .then(data => {
                 this.eventos = data.events
                 console.log(this.eventos);
+                this.checkbox = [...new Set(data.events.map(evento=>evento.category))]
+                console.log(this.checkbox);
+
             })
+
             .catch(err => console.log(err))
+
     },
     methods: {
 
     },
     computed: {
+        fitrosCruzados:function(){
+            let arregloFiltrado = this.inputCheck.length>0? this.eventos.filter(evento => this.inputCheck.includes(evento.category)):this.eventos
+            this.eventosFiltrados = this.texto ==""? arregloFiltrado: arregloFiltrado.filter(evento => evento.name.toLowerCase().includes(this.texto.toLowerCase().trim()))
+        }
 
     }
 }).mount("#app")
